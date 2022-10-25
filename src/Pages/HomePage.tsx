b793146d7b71eb496ebed38655b7fbea6b7fc1ff
Blazecode200 <69413640+adisagar2003@users.noreply.github.com>
2022-10-25 19:30:25 -0230

    Update HomePage.tsx

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -4,13 +4,21 @@ import Cookies from 'universal-cookie';
 let cookies =new Cookies();
 const checkCookie = () =>{
   if (cookies.get('access_token')){
-    console.log('cookie found nigga');
+
   }
 }
 function HomePage() {
   return (
-    <div>
+    <div class="flex">
         <Sidebar />
+        <div class="p-20">
+        <div class="md:flex md:flex-row flex flex-col relative left-80 p-20 border-red w-120 h-120 bg-slate-300 ">
+          <div>a</div>
+          <div>b</div>
+          <div>c</div>
+          <div>d</div>
+        </div>
+        </div>
     </div>
   )
 }
